1dff72d67ac20aa809350bca9b4989f6b24519cf
kevin <bellkevin677@gmail.com>
2021-08-24 15:50:04 -0500

    added more material card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,64 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
+import React, { useEffect, useState } from "react";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
-const Card = (props) => (
-    <div style={{ color: '#fff' }}>
-      <h2>Title</h2>
-      <div>Date</div>
-      <div>
-        Info
-      </div>
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+    boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+    backgroundColor: "#fafafa",
+  },
+  media: {
+    height: 300,
+  },
+});
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("url here")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Container>
+        <Typography
+          color="textPrimary"
+          gutterBottom
+          variant="h2"
+          align="center"
+        >
+          Example{" "}
+        </Typography>
+        <Grid container spacing={3}>
+          {data.map((param) => (
+            <Grid item xs={12} sm={4} key={}>
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography color="primary" variant="h5">
+                    {key.map}
+                  </Typography>
+                  <Typography color="textSecondary" variant="subtitle2">
+                    {key.map}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
-  )
+  );
+}
 
-export default Card
+export default App;
